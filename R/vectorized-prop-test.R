f6c67bc3d793c37e486aae4a3dff070a18739850
Dave Robinson <drobinson@stackoverflow.com>
2016-09-26 13:13:13 -0400

    Changed vectorized prop test to return a table with confidence intervals and estimates
    Version bump

--- R/vectorized-prop-test.R
+++ R/vectorized-prop-test.R
@@ -11,15 +11,45 @@
 #' @param d Number of failures in second condition
 #'
 #' @export
-vectorized_prop_test <- function(a, b, c, d) {
-  # if any values are < 30, use exact
-  exact <- (a < 30 | b < 30 | c < 30 | d < 30)
+vectorized_prop_test <- function(x1, n1, x2, n2, conf.level = .95) {
+  a <- x1
+  b <- n1 - x1
+  c <- x2
+  d <- n2 - x2
 
-  result <- rep(NA, length(a))
-  result[exact] <- vectorized_prop_test_exact(a[exact], b[exact], c[exact], d[exact])
-  result[!exact] <- vectorized_prop_test_approx(a[!exact], b[!exact], c[!exact], d[!exact])
+  # if any values are < 20, use Fisher's exact test
+  exact <- (a < 20 | b < 20 | c < 20 | d < 20)
 
-  result
+  pvalue <- rep(NA, length(a))
+
+  if (any(exact)) {
+    pvalue[exact] <- vectorized_prop_test_exact(a[exact], b[exact], c[exact], d[exact])
+  }
+  if (any(!exact)) {
+    pvalue[!exact] <- vectorized_prop_test_approx(a[!exact], b[!exact], c[!exact], d[!exact])
+  }
+
+  mu1 <- a / (a + b)
+  mu2 <- c / (c + d)
+
+  ## confidence interval
+  alpha2 <- (1 - conf.level) / 2
+  DELTA <- mu2 - mu1
+  WIDTH <- qnorm(alpha2)
+  alpha <- (a + .5) / (a + b + 1)
+  beta <- (c + .5) / (c + d + 1)
+
+  n <- n1 + n2
+  YATES <- pmin(.5, abs(DELTA) / sum(1 / n1 + 1 / n2))
+
+  # TODO: add yates correction
+  z <- qnorm((1 + conf.level) / 2)
+  WIDTH <- z * sqrt(mu1 * (1 - mu1) / n1 + mu2 * (1 - mu2) / n2)
+
+  dplyr::data_frame(estimate = DELTA,
+                    conf.low = pmax(DELTA - WIDTH, -1),
+                    conf.high = pmin(DELTA + WIDTH, 1),
+                    p.value = pvalue)
 }
 
 
